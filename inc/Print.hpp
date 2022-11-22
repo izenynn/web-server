8520ebaef63afe2136896f87855dc039726fd46f
jcarrete <jcarrete@student.42madrid.com>
2022-11-22 08:01:32 +0100

    Added Print Header
    Updated Gitignore.

---
+++ inc/Print.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef __PRINT_HPP__
+#define __PRINT_HPP__
+
+/**	INCLUDES ----------------------------------- */
+
+# include <iostream>
+# include <ostream>
+
+# include "Colors.hpp"
+
+/**	METHODS   ---------------------------------- */
+
+
+#endif /** __PRINT_HPP__ */
