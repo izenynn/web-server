ef4d95e0185f4a662ba6206a5bf0d56011676911
user <user@user.com>
2023-01-23 21:54:10 +0100

    fix(Response): change status codes and mime types map to static for performance reasons

--- src/server/Response.hpp
+++ src/server/Response.hpp
@@ -8,16 +8,27 @@
 # include <map>
 # include <string>
 
+# include <config/RequestConfig.hpp>
+
 /** CLASS -------------------------------------- */
 
 namespace webserv {
 
 class Response {
 	public:
+		Response( const RequestConfig & config, int status_code );
+		~Response( void );
 	private:
-		std::map<int, std::string>			_status_codes;
-		std::map<std::string, std::string>	_mime_types;
-}
+		Response( void ); // not necessary
+		Response( const Response & other ); // not necessary
+		Response & operator=( const Response & other ); // not necessary
+
+		static std::map<int, std::string>			_status_codes;
+		static std::map<std::string, std::string>	_mime_types;
+
+		static std::map<int, std::string>			initStatusCodes( void );
+		static std::map<std::string, std::string>	initMimeTypes( void );
+};
 
 } /** namespace webserv */
 
