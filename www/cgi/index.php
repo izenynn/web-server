44d694ea47874d46b74419f4d3b4715cd619a52e
user <user@user.com>
2023-01-31 12:15:52 +0100

    chore: add php index and install php-cgi

---
+++ www/cgi/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>PHP Test</title>
+	</head>
+	<body>
+		<h1>PHP echo</h1>
+		<?php echo '<p>Hello World</p>'; ?>
+		<br />
+		<h1>PHP Info</h1>
+		<?php phpinfo(); ?>
+	</body>
+</html>
