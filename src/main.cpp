6df34ba40677a305cc77ff235554a0515675a532
izenynn <me@izenynn.com>
2023-03-02 00:38:07 +0100

    style: main

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,40 @@
 #include <cstring> // std::strcmp()
 
-#include <types/nullptr_t.hpp>
-#include <server/Server.hpp>
-#include <utils/log.hpp>
+#include "types.hpp"
+#include "utils/log.hpp"
+#include "server/Server.hpp"
 
 int main( int argc, char * argv[] ) {
-	int					ret = 0;
-	webserv::Server *	server = webserv::nullptr;
+  // too many args
+  if ( argc > 2 ) {
+    LOG_FAILURE( "Error: too many arguments" );
+    LOG_FAILURE( "Usage: " << argv[0] << " [CONF FILE]" );
+    return ( 1 );
+  }
 
-	// too many args
-	if ( argc > 2 ) {
-		LOG_FAILURE( "Error: too many arguments" );
-		LOG_FAILURE( "Usage: " << argv[0] << " [CONF FILE]" );
-		return ( 1 );
-	}
+  // help message
+  if ( argc == 2 ) {
+    if ( 0 == std::strcmp( "-h", argv[1] )
+        || 0 == std::strcmp( "--help", argv[1] ) ) {
+      LOG_INFO( "Usage: " << argv[0] << " [CONF FILE]" );
+      return ( 0 );
+    }
+  }
 
-	// help message
-	if ( argc == 2 && ( 0 == std::strcmp( "-h", argv[1] ) || 0 == std::strcmp( "--help", argv[1] ) ) ) {
-		LOG_INFO( "Usage: " << argv[0] << " [CONF FILE]" );
-		return ( 0 );
-	}
+  // web server
+  webserv::unique_ptr<webserv::Server> server( new webserv::Server() );
 
-	// web server
-	server = new webserv::Server();
+  LOG_INFO( "loading config..." );
+  try {
+    if ( argc == 2 ) server->load( argv[1] );
+    else             server->load();
+  } catch ( std::exception & e ) {
+    LOG_ERROR( e.what() );
+    return ( 1 );
+  }
 
-	LOG_INFO( "loading config..." );
-	try {
-		if ( argc == 2 ) server->load( argv[1] );
-		else             server->load();
-	} catch ( std::exception & e ) {
-		delete server;
-		LOG_ERROR( e.what() );
-		return ( 1 );
-	}
+  LOG_INFO( "starting..." );
+  int ret = server->start();
 
-	//server->print();
-
-	LOG_INFO( "starting..." );
-	ret = server->start();
-
-	delete server;
-
-	//system( "sleep 3600" );
-
-	return ( ret );
+  return ( ret );
 }
